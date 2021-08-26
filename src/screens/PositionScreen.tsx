05a3c9e8262c9e2877bdc08d51fadb9a3c8cec37
Acxel Morales <acxelmorales97@gmail.com>
2021-08-25 22:41:55 -0500

    Position absolute

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -4,8 +4,11 @@ import { View, StyleSheet } from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.limeDarkBox} />
+      <View style={styles.greenBox} />
       <View style={styles.purpleBox} />
-      <View style={styles.orangeBox} />
+      <View style={styles.limeBox} />
+      <View style={styles.pinkBox} />
     </View>
   );
 };
@@ -13,25 +16,50 @@ export const PositionScreen = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#28C4D9',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  greenBox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#8BE072',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    right: 0,
   },
   purpleBox: {
     width: 100,
     height: 100,
-    backgroundColor: '#5856D6',
+    backgroundColor: '#7F7EE0',
     borderWidth: 10,
     borderColor: 'white',
-    top: 100,
-    left: 100,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
   },
-  orangeBox: {
+  limeBox: {
     width: 100,
     height: 100,
-    backgroundColor: '#F0A23B',
+    backgroundColor: '#D6E067',
     borderWidth: 10,
     borderColor: 'white',
-    top: -50,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  pinkBox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#E051C6',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  limeDarkBox: {
+    backgroundColor: '#E0C95C',
+    ...StyleSheet.absoluteFillObject
   },
 });
