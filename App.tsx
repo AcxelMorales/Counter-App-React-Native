e03c33f5bcc986ce01e24a34f60c46cf3f109d6d
Acxel Morales <acxelmorales97@gmail.com>
2021-08-25 21:32:26 -0500

    Dimensions example

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 
 // import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
-import { CounterScreen } from './src/screens/CounterScreen';
+// import { CounterScreen } from './src/screens/CounterScreen';
+// import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { DimensionsScreen } from './src/screens/DimensionsScreen';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <CounterScreen />
+    <SafeAreaView style={{ flex: 1 }}>
+      <DimensionsScreen />
     </SafeAreaView>
   );
 };
